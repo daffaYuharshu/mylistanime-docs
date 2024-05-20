18f4969da81a7e22d48af9c0d0901438693b3480
daffaYuharshu <daffayuharshu@gmail.com>
2024-05-20 11:08:04 +0700

    remove express static public

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const path = require("path");
 const swaggerUI = require("swagger-ui-express");
 
 const apiDocumentation = require("./openapi.json");
@@ -7,7 +6,6 @@ const port = 3001;
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '../public')));
 app.use("/", swaggerUI.serve, swaggerUI.setup(apiDocumentation));
 
 app.listen(port, () => {

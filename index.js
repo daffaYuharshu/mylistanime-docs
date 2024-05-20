ef0626380534485ca75db67f411e6ec50220a822
daffaYuharshu <daffayuharshu@gmail.com>
2024-05-20 10:44:13 +0700

    add express.statis

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const swaggerUI = require("swagger-ui-express");
 
 const apiDocumentation = require("./openapi.json");
@@ -6,6 +7,7 @@ const port = 3001;
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, '../public')));
 app.use("/", swaggerUI.serve, swaggerUI.setup(apiDocumentation));
 
 app.listen(port, () => {
